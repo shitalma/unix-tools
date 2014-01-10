a5cfdc9bc2efef3aa841d548088e6e0007ae9e95
Shital <shitalma@thoughtworks.com>
2014-01-11 00:43:12 +0530

    cut functionality done with multiple options

--- src/shitalma/unixtools/cut/Cut.java
+++ src/shitalma/unixtools/cut/Cut.java
@@ -4,12 +4,23 @@ public class Cut {
     public static void main(String[] args) {
         CutOperations operations = new CutOperations();
         String fileContent;
-        String seperator = args[1].substring(2,3);
-        String data = args[0].substring(2,3);
-        int number = Integer.parseInt(data);
-        fileContent = operations.readFile(args[2]);
-        operations.cut_specific_character(number, fileContent);
-        operations.cut_specific_field(number, fileContent);
-        operations.cut_using_specific_field_and_seperator(number, seperator, fileContent);
+        if(args.length == 3) {           // for delimiter
+            String seperator = args[1].substring(2, 3);
+            int number = Integer.parseInt(args[0].substring(2,3));
+            fileContent = operations.readFile(args[2]);
+            operations.cut_using_specific_field_and_seperator(number, seperator, fileContent);
+        }
+        try {
+            if(args.length == 2) {           // for field and character
+                fileContent = operations.readFile(args[1]);
+                int number = Integer.parseInt(args[0].substring(2,3));
+                String character = args[0].substring(1, 2);
+                if (0 == character.compareTo("f"))
+                    operations.cut_specific_field(number, fileContent);
+                else operations.cut_specific_character(number, fileContent);
+            }
+        } catch(Exception e) {
+            System.err.println("Have error for reading file");
+        }
     }
 }
--- src/shitalma/unixtools/cut/CutOperations.java
+++ src/shitalma/unixtools/cut/CutOperations.java
@@ -9,13 +9,16 @@ public class CutOperations extends MyFileReader {
     }
     public void cut_specific_field(int fieldNumber,String text) {
         String[] result = text.split("\r\n");
-        for (int i = 0; i < result.length-1; i++)
-            System.out.println(result[i].split(" ")[fieldNumber]);
+        for (int i = 0; i < result.length; i++){
+            String[] data = result[i].split(" ");
+            if(data.length < fieldNumber)
+                System.out.println("\n");
+            else System.out.println(data[fieldNumber-1]);
+        }
     }
     public void cut_using_specific_field_and_seperator(int fieldNumber,String seperator,String text) {
         String[] result = text.split("\r\n");
         for (int i = 0; i < result.length-1; i++)
             System.out.println(result[i].split(seperator)[fieldNumber]);
     }
-
 }
