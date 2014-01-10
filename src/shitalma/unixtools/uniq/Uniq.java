06fa44e600407ad86bda2c93e45ef806a14ac9ba
Shital <shitalma@thoughtworks.com>
2014-01-10 22:27:03 +0530

    uniq functionality done with file scanner

--- src/shitalma/unixtools/uniq/Uniq.java
+++ src/shitalma/unixtools/uniq/Uniq.java
@@ -1,19 +1,10 @@
 package shitalma.unixtools.uniq;
-<<<<<<< HEAD
+import shitalma.unixtools.MyFileReader;
 
-public class Uniq {
-    public static void main(String[] args) {
-        UniqOperations operations = new UniqOperations();
-        String fileData = operations.readFile(args[0]);
-        operations.display_uniq(fileData);
-=======
-import java.io.BufferedReader;
-import java.io.FileReader;
-
-class operations {
+class Operations extends MyFileReader{
     public void display_uniq(String text) {
         String[] result = text.split("\r\n");
-        for(int i =1; i< result.length-1; i++){
+        for(int i =0 ; i< result.length-1; i++){
             if(0 != result[i].compareTo(result[i + 1]))
                 System.out.println(result[i]);
         }
@@ -21,18 +12,8 @@ class operations {
 }
 public class Uniq {
     public static void main(String[] args) {
-        operations operations = new operations();
-        BufferedReader bufferedReader;
-        String fileContent = null;
-        try {
-            bufferedReader = new BufferedReader(new FileReader(args[0]));
-            String currentLine;
-            while ((currentLine = bufferedReader.readLine()) != null)
-                fileContent += "\r\n" + currentLine;
-        } catch (Exception e) {
-            System.out.println("Error");
-        }
-        operations.display_uniq(fileContent);
->>>>>>> origin/master
+        Operations operations = new Operations();
+        String fileData = operations.readFile(args[0]);
+        operations.display_uniq(fileData);
     }
 }
