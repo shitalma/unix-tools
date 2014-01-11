76a2ab58f278439ffca5a9b373cd233d98c54548
Shital <shitalma@thoughtworks.com>
2014-01-11 09:53:26 +0530

    reduce space functionality going to write result back into new file using file reader

--- src/shitalma/unixtools/reduceSpace/ReduceSpace.java
+++ src/shitalma/unixtools/reduceSpace/ReduceSpace.java
@@ -1,7 +1,9 @@
 package shitalma.unixtools.reduceSpace;
 
+import java.io.IOException;
+
 public class ReduceSpace {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ReduceSpaceOperations rso = new ReduceSpaceOperations();
         String fileContent = rso.readFile(args[0]);
         rso.reduceSpace(fileContent);
--- src/shitalma/unixtools/reduceSpace/ReduceSpaceOperations.java
+++ src/shitalma/unixtools/reduceSpace/ReduceSpaceOperations.java
@@ -1,11 +1,26 @@
 package shitalma.unixtools.reduceSpace;
 
 import shitalma.unixtools.MyFileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class ReduceSpaceOperations extends MyFileReader{
-    public void reduceSpace(String fileContent) {
+    BufferedWriter writeTo;
+    public ReduceSpaceOperations() {
+        try {
+            writeTo = new BufferedWriter(new FileWriter("d.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void reduceSpace(String fileContent) throws IOException {
         String[] result = fileContent.split("\r\n");
-        for (int i = 0; i < result.length-1; i++)
-            System.out.println(result[i].replaceAll("\\s+"," "));
+        for (int i = 0; i < result.length-1; i++){
+            String data = result[i].replaceAll("\\s+", " ");
+            System.out.println(data);
+            writeTo.write(data+"\r\n");
+        }
+        writeTo.close();
     }
 }
