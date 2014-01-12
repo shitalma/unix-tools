a05f89919f16e1b866225516814790c93e699c95
Shital <shitalma@thoughtworks.com>
2014-01-12 13:37:19 +0530

    test case run for the character count in WC.

--- src/shitalma/unixtools/wc/WcLib.java
+++ src/shitalma/unixtools/wc/WcLib.java
@@ -8,18 +8,13 @@ public class WcLib {
                 noOfLines += i.split("\n").length;
             return noOfLines;
         }
-
         public int countWords(String text) {
             int noOfWords = 0;
             String[] result = text.split("\r\n");
-            int noOfLines = 0;
-            for (String i : result)
-                noOfLines += i.split("\n").length;
             for (String item : result)
-                noOfWords += item.split(" ").length;
-            return noOfWords+noOfLines-1;
+                noOfWords += item.split("\\s").length;
+            return noOfWords;
         }
-
         public int countCharacters(String text) {
             return text.length();
         }
--- test/shitalma/unixtools/wc/WcLibTest.java
+++ test/shitalma/unixtools/wc/WcLibTest.java
@@ -4,12 +4,26 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class WcLibTest {
+    final String input = "I am Shital Mane.\r\nI am from Satara.\r\nI am fine?";
     @Test
     public void testCountLines() throws Exception {
-        String input = "I am Shital Mane.\n I am from Satara.\nI am fine?";
         WcLib wc = new WcLib();
         int expected = 3;
         int actual = wc.countLines(input);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testCountWords() throws Exception {
+        WcLib wc = new WcLib();
+        int expected = 11;
+        int actual = wc.countWords(input);
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testCountCharacters() throws Exception {
+        WcLib wc = new WcLib();
+        int expected = 48;
+        int actual = wc.countCharacters(input);
         assertEquals(expected, actual);
     }
 }
