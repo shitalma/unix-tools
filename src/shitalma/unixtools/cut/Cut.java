ea25db8924faca31aab6a24605de792932a5d50f
Shital <shitalma@thoughtworks.com>
2014-01-13 11:37:19 +0530

    cut functionality done with test cases and using string builder.

--- src/shitalma/unixtools/cut/Cut.java
+++ src/shitalma/unixtools/cut/Cut.java
@@ -1,23 +1,33 @@
 package shitalma.unixtools.cut;
 
+import shitalma.unixtools.FileReader;
+
 public class Cut {
     public static void main(String[] args) {
-        CutOperations operations = new CutOperations();
+        CutLib operations = new CutLib();
+        FileReader readContent = new FileReader();
         String fileContent;
         if(args.length == 3) {           // for delimiter
             String seperator = args[1].substring(2, 3);
             int number = Integer.parseInt(args[0].substring(2,3));
-            fileContent = operations.readFile(args[2]);
-            operations.cut_using_specific_field_and_seperator(number, seperator, fileContent);
+            fileContent = readContent.readFile(args[2]);
+            String data = operations.cutUsingSpecificFieldAndSeperator(number, seperator, fileContent);
+            String[] result = data.split("\r\n");
+            for (java.lang.Object o : result)
+                System.out.println(o);
         }
         try {
             if(args.length == 2) {           // for field and character
-                fileContent = operations.readFile(args[1]);
+                fileContent = readContent.readFile(args[1]);
                 int number = Integer.parseInt(args[0].substring(2,3));
                 String character = args[0].substring(0, 2);
+                String data;
                 if (0 == character.compareTo("-f"))
-                    operations.cut_specific_field(fileContent);
-                else operations.cut_specific_character(number, fileContent);
+                    data = operations.cutSpecificField(fileContent);
+                else data = operations.cutSpecificCharacter(number, fileContent);
+                String[] result = data.split("\r\n");
+                for (java.lang.Object o : result)
+                    System.out.println(o);
             }
         } catch(Exception e) {
             System.err.println("Have error for reading file");
---
+++ src/shitalma/unixtools/cut/CutLib.java
@@ -0,0 +1,29 @@
+package shitalma.unixtools.cut;
+
+public class CutLib {
+        public String cutSpecificCharacter(int number, String text) {
+            String[] result = text.split("\r\n");
+            StringBuilder charactersAfterCutting = new StringBuilder();
+            for (int i = 0; i < result.length; i++)
+                charactersAfterCutting.append(result[i].substring(number-1,number)).append("\r\n");
+            return charactersAfterCutting.toString();
+        }
+        public String cutSpecificField(String text) {
+            String[] result = text.split("\r\n");
+            StringBuilder fields = new StringBuilder();
+            for (String aResult : result)
+                fields.append(aResult).append("\r\n");
+            return fields.toString();
+        }
+        public String cutUsingSpecificFieldAndSeperator(int fieldNumber,String seperator,String text) {
+            String[] result = text.split("\r\n");
+            StringBuilder cut = new StringBuilder();
+            for (String aResult : result) {
+                String[] data = aResult.split(seperator);
+                if (data.length < fieldNumber)
+                    cut.append("\r\n");
+                else cut.append(data[fieldNumber - 1]).append("\r\n");
+            }
+            return cut.toString();
+        }
+}
