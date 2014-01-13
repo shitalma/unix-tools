bbdbbe307479211089300923ff02ce6315dcbbae
Shital <shitalma@thoughtworks.com>
2014-01-13 10:03:19 +0530

    uniq functionality done with test cases

--- src/shitalma/unixtools/uniq/Uniq.java
+++ src/shitalma/unixtools/uniq/Uniq.java
@@ -1,9 +1,12 @@
 package shitalma.unixtools.uniq;
 
+import shitalma.unixtools.FileReader;
+
 public class Uniq {
     public static void main(String[] args) {
-        UniqOperations operations = new UniqOperations();
-        String fileData = operations.readFile(args[0]);
-        operations.display_uniq(fileData);
+        UniqLib operations = new UniqLib();
+        FileReader readContent = new FileReader();
+        String fileData = readContent.readFile(args[0]);
+        operations.uniqLines(fileData);
     }
 }
---
+++ src/shitalma/unixtools/uniq/UniqLib.java
@@ -0,0 +1,14 @@
+package shitalma.unixtools.uniq;
+
+public class UniqLib {
+        public String uniqLines(String text) {
+            String[] lines = text.split("\r\n");
+            StringBuilder stringBuilder = new StringBuilder();
+            for(int i =0; i< lines.length-1; i++){
+                if(!(lines[i].equalsIgnoreCase(lines[i + 1])))
+                    stringBuilder.append(lines[i]).append("\r\n");
+            }
+            stringBuilder.append(lines[lines.length-1]);
+            return stringBuilder.toString();
+        }
+}
