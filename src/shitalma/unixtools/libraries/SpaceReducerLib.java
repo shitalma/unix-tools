d3ed8adceef84526836f02b9953aa1478e443794
Shital <shitalma@thoughtworks.com>
2014-01-15 08:03:08 +0530

    all shell files runnig for all main program

--- src/shitalma/unixtools/libraries/SpaceReducerLib.java
+++ src/shitalma/unixtools/libraries/SpaceReducerLib.java
@@ -1,11 +1,26 @@
 package shitalma.unixtools.libraries;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class SpaceReducerLib {
-        public String spaceReducer(String fileContent) {
+        BufferedWriter writeTo;
+        public SpaceReducerLib() {
+            try {
+                writeTo = new BufferedWriter(new FileWriter("d.txt"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        public String spaceReducer(String fileContent) throws IOException {
             String[] result = fileContent.split("\r\n");
             StringBuilder data = new StringBuilder();
-            for (int i = 0; i < result.length; i++)
+            for (int i = 0; i < result.length; i++){
                 data.append(result[i].replaceAll("\\s+", " ")).append("\r\n");
+                writeTo.write(data+"\r\n");
+            }
+            writeTo.close();
             return data.toString();
         }
 }
