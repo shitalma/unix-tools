debb9b147523cadb588725bc7e232504da01dd74
Shital <shitalma@thoughtworks.com>
2014-01-15 23:42:21 +0530

    all test cases done for uniq.

--- src/shitalma/unixtools/libraries/UniqLib.java
+++ src/shitalma/unixtools/libraries/UniqLib.java
@@ -1,15 +1,19 @@
 package shitalma.unixtools.libraries;
 
 public class UniqLib {
-    public StringBuilder uniqData(String input) {
-        String data[] = input.split("\n");
-        int length = data.length-1;
+    public String text;
+    public UniqLib(String text) {
+        this.text = text;
+    }
+    public StringBuilder uniqData() {
+        String data[] = text.split("\r\n");
+        int length = data.length;
         StringBuilder stringBuilder = new StringBuilder();
         for (int count = 0; count < data.length - 1; count++) {
             if (!(data[count].equalsIgnoreCase(data[count + 1])))
-                stringBuilder.append(data[count]);
+                stringBuilder.append(data[count]).append("\r\n");
         }
-        stringBuilder.append(data[length]);
+        stringBuilder.append(data[length-1]);
         return stringBuilder;
     }
 }
--- test/shitalma/unixtools/uniq/UniqLibTest.java
+++ test/shitalma/unixtools/uniq/UniqLibTest.java
@@ -6,26 +6,39 @@ import shitalma.unixtools.libraries.UniqLib;
 import static junit.framework.Assert.assertEquals;
 
 public class UniqLibTest {
+    final String input = "I am here.\r\nI am here.\r\nMy name is Shital\r\n How are You\r\n How are You";
     @Test
-    public void testuniqData() throws Exception {
-        String input = "I am here.\r\nI am here.\r\nMy name is Shital\r\n How are You\r\n How are You";
-        UniqLib uniq = new UniqLib();
+    public void uniqForMultipleLines() throws Exception {
+        UniqLib uniq = new UniqLib(input);
         StringBuilder expected = new StringBuilder();
         StringBuilder actual;
 
-        expected.append("I am here.\r\nMy name is Shital\r\n How are You\r\n");
-        actual = uniq.uniqData(input);
-        assertEquals(expected, actual);
+        expected.append("I am here.\r\nMy name is Shital\r\n How are You");
+        actual = uniq.uniqData();
+
+        assertEquals(expected.toString(), actual.toString());
     }
 
     @Test
-    public void test_uniqData() throws Exception {
-        String input = "I am here.\r\nI am here.\r\nMy name is Shital\r\n How are You\r\n How are You\r\n";
-        UniqLib uniq = new UniqLib();
+    public void uniqForMultipleEmptyLinesAtTheEnd() throws Exception {
+        UniqLib uniq = new UniqLib(input+"\r\n");
         StringBuilder expected = new StringBuilder();
         StringBuilder actual;
-        expected.append("I am here.\r\nMy name is Shital\r\n How are You\r\n");
-        actual = uniq.uniqData(input);
-        assertEquals(expected, actual);
+
+        expected.append("I am here.\r\nMy name is Shital\r\n How are You");
+        actual = uniq.uniqData();
+
+        assertEquals(expected.toString(), actual.toString());
+    }
+    @Test
+    public void uniqWillReturnEmptyStringWhenInputIsEmpty() throws Exception {
+        UniqLib uniq = new UniqLib("");
+        StringBuilder expected = new StringBuilder();
+        StringBuilder actual;
+
+        expected.append("");
+        actual = uniq.uniqData();
+
+        assertEquals(expected.toString(), actual.toString());
     }
 }
