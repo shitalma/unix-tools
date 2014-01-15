405ab6512aae432078fe3d2cd809f3ce3ef2d637
Shital <shitalma@thoughtworks.com>
2014-01-15 23:56:22 +0530

    test cases done for space reducer.

--- test/shitalma/unixtools/reduceSpace/SpaceReducerLibTest.java
+++ test/shitalma/unixtools/reduceSpace/SpaceReducerLibTest.java
@@ -10,8 +10,32 @@ public class SpaceReducerLibTest {
     public void spaceReducerForMultipleLines() throws Exception {
         String input = "I am                here.\r\n My name     is Shital\r\n How         are You";
         SpaceReducerLib spr = new SpaceReducerLib();
+
         String expected = "I am here.\r\n My name is Shital\r\n How are You";
         String actual = spr.spaceReducer(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void spaceReducerForSingleLine() throws Exception {
+        String input = "I          am                here.";
+        SpaceReducerLib spr = new SpaceReducerLib();
+
+        String expected = "I am here.";
+        String actual = spr.spaceReducer(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void spaceReducerForEmptyInput() throws Exception {
+        String input = "";
+        SpaceReducerLib spr = new SpaceReducerLib();
+
+        String expected = "";
+        String actual = spr.spaceReducer(input);
+
         assertEquals(expected, actual);
     }
 }
