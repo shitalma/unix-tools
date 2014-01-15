04c1516bbda0f25c2b09eeb69aa54555a7c33ccd
Shital <shitalma@thoughtworks.com>
2014-01-15 22:37:09 +0530

    all test cases done for head.

--- src/shitalma/unixtools/libraries/HeadLib.java
+++ src/shitalma/unixtools/libraries/HeadLib.java
@@ -13,8 +13,9 @@ public class HeadLib {
         String[] lines = text.split("\r\n");
         if (lines.length < limit) limit = lines.length;
         StringBuilder result = new StringBuilder();
-        for (int i = 0; i < limit; i++)
+        for (int i = 0; i < limit-1; i++)
             result.append(lines[i]).append("\r\n");
+        result.append(lines[limit-1]);
         return result.toString();
     }
 }
--- test/shitalma/unixtools/head/HeadLibTest.java
+++ test/shitalma/unixtools/head/HeadLibTest.java
@@ -6,12 +6,49 @@ import shitalma.unixtools.libraries.HeadLib;
 import static org.junit.Assert.assertEquals;
 
 public class HeadLibTest {
+    final String input = "I am here.\r\n My name is Shital\r\n How are You\r\n"+
+            "manali\r\nkajal\r\nshabrin\r\nshweta\r\nkavita\r\nsayali\r\nprajakta\r\nsamiksha";
     @Test
-    public void testDisplay_head() throws Exception {
-        String input = "I am here.\r\n My name is Shital\r\n How are You";
+    public void displayHeadWhenNumberIsLessThanTen() throws Exception {
         HeadLib head = new HeadLib(2, input);
-        String expected = "I am here.\r\n My name is Shital\r\n";
+        String expected = "I am here.\r\n My name is Shital";
         String actual = head.display_head();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void displayHeadWhenNumberOfLinesAreByeDefaultTen() throws Exception {
+        HeadLib head = new HeadLib(10, input);
+        String expected = "I am here.\r\n My name is Shital\r\n How are You\r\nmanali\r\n"+
+                "kajal\r\nshabrin\r\nshweta\r\nkavita\r\nsayali\r\nprajakta";
+        String actual = head.display_head();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void displayHeadWhenNumberOfLinesMoreThanTen() throws Exception {
+        HeadLib head = new HeadLib(13, input);
+        String expected = "I am here.\r\n My name is Shital\r\n How are You\r\nmanali\r\n"+
+                "kajal\r\nshabrin\r\nshweta\r\nkavita\r\nsayali\r\nprajakta\r\nsamiksha";
+        String actual = head.display_head();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void displayHeadWhenNumberOfLinesMoreThanHundread() throws Exception {
+        HeadLib head = new HeadLib(130, input);
+        String expected = "I am here.\r\n My name is Shital\r\n How are You\r\nmanali\r\n"+
+                "kajal\r\nshabrin\r\nshweta\r\nkavita\r\nsayali\r\nprajakta\r\nsamiksha";
+        String actual = head.display_head();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void displayHeadWhenNumberOfLinesWhenTextIsEmpty() throws Exception {
+        HeadLib head = new HeadLib(1, "");
+        String expected = "";
+        String actual = head.display_head();
+        System.out.println(actual);
         assertEquals(expected, actual);
     }
 }
