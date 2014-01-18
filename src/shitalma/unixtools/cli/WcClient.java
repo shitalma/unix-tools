208629193ba8f05a1305c6d4d9bf9991a1117f9c
shitalma <shitalmane639@gmail.com>
2014-01-18 13:15:27 +0530

    wc functionality done with handing multiple inputs

--- src/shitalma/unixtools/cli/WcClient.java
+++ src/shitalma/unixtools/cli/WcClient.java
@@ -6,11 +6,49 @@ import shitalma.unixtools.libraries.WcLib;
 public class WcClient {
     public static void main(String[] args) {
         FileReader readContent = new FileReader();
-        String fileData = readContent.readFile(args[0]);
+        WcClient wc = new WcClient();
+        String[] processArgs = wc.getParameters(args);
+        String fileData = readContent.readFile(processArgs[3]);
         WcLib operations = new WcLib(fileData);
-        int lines = operations.countLines();
-        int words = operations.countWords();
-        int characters = operations.countCharacters();
-        System.out.println(lines + " " + words + " " + characters + " " + args[0]);
+        if(processArgs[0] != null) {
+            int characters = operations.countCharacters();
+            System.out.print(characters + " ");
+        }
+        if(processArgs[1] != null){
+            int words = operations.countWords();
+            System.out.print(words+ " ");
+        }
+        if(processArgs[2] != null){
+            int lines = operations.countLines();
+            System.out.print(lines + " ");
+        }
+        System.out.print(processArgs[3].substring(processArgs[3].length()-5,processArgs[3].length()));
+    }
+    String[] getParameters(String[] args) {
+        String[] options = new String[4];
+        for (int i = 0; i < args.length; i++) {
+            if (WcClient.isCharacters(args[i]))
+                options[0] = args[i];
+            if (WcClient.isWords(args[i]))
+                options[1] = args[i];
+            if (WcClient.isLines(args[i]))
+                options[2] = args[i];
+            if (!WcClient.isFile(args[i]))
+                options[3] = args[i];
+        }
+        return options;
+    }
+
+    static boolean isCharacters(String arg) {
+        return arg.matches("-c");
+    }
+    static boolean isWords(String arg) {
+        return arg.matches("-w");
+    }
+    static boolean isLines(String arg) {
+        return arg.matches("-l");
+    }
+    static boolean isFile(String arg) {
+        return arg.matches("-.*");
     }
 }
